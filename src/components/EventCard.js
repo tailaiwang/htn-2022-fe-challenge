947a7f6356b3674853c5a775a05281c65858b696
tailaiwang <tailaiwang01@gmail.com>
2022-02-14 21:15:36 -0500

    leftover code

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -2,22 +2,14 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import moment from "moment";
 
-
-
-
-export default function Event({
-  close: closeWrapper,
-  related,
-  event,
-}) {
-
+export default function Event({ close: closeWrapper, related, event }) {
   const [curEvent, setCurEvent] = useState(event);
 
   const newCard = (id) => {
     let updatedValue = related[id - 1];
     setCurEvent((curEvent) => updatedValue);
     console.log("Hi");
-  }
+  };
 
   return (
     <Wrapper>
@@ -32,7 +24,12 @@ export default function Event({
         Related Event(s):
         {curEvent.related_events.map((event, index) => {
           return (
-            <Button onClick={() => {newCard(event)}} key={index}>
+            <Button
+              onClick={() => {
+                newCard(event);
+              }}
+              key={index}
+            >
               {event}
             </Button>
           );
